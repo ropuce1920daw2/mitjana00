78a52d9bf4d39e67a279defa5d4556b04c3d9a1f
ropuce1920daw2 <ropuce1920daw2@gmail.com>
2019-12-15 18:30:17 +0100

    Conflicte 1 entre br0 i master

--- mitjana00.c
+++ mitjana00.c
@@ -1,4 +1,4 @@
-//Autor: dacomo1920daw2
+﻿//Autor: dacomo1920daw2
 //Data i hora: 201911221223
 
 #include <stdio.h>
@@ -23,5 +23,6 @@ int main() {
 	}
 	mitjana=suma/numero_repeticions;
 	printf("La mitjana resultant és: %.2f\n",mitjana);	
+	printf("Operació finalitzada amb èxit\n");
 	return 0;
 }
